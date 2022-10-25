917797522973d31ed93cf6ce2008f3baeb5a4ca0
yanghaoran <1064137887@qq.com>
2022-10-25 21:02:10 +0800

    添加oss未启用获取报错

--- src/system/oss/oss.service.ts
+++ src/system/oss/oss.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import * as OSS from 'ali-oss'
 import { envConfig } from 'src/common/config';
+import { LogManager } from '../log/log.manager';
 import { OssPutSuccessResponseDto } from './dto/oss-put-success-response.dto';
 
 @Injectable()
 export class OssService {
     private _client: OSS;
+    public get client() {
+        if (!envConfig.enable_oss) {
+            const logFormat = `****************************************************************
+    OSS environment config was disabled
+ ****************************************************************`;
+            LogManager.error(logFormat);
+            return null;
+        }
+        return this._client;
+    }
 
     constructor() {
         if(envConfig.enable_oss) {
@@ -17,7 +28,7 @@ export class OssService {
         const basePath = envConfig.ossBaseFilePath;
         const fileName = file.originalname;
         const putPath = `${basePath}/${filePath}/${Date.now()}${fileName}`;
-        let result = await this._client.put(putPath, file.buffer);
+        let result = await this.client.put(putPath, file.buffer);
         let res = new OssPutSuccessResponseDto();
         res.url = result.url;
         res.name = result.name;
