1808d8d810bfd8f2159d4c5afdb1d7712b1323e9
yanghaoran <1064137887@qq.com>
2023-02-22 15:46:41 +0800

    exceljs包支持流式写入，之前错怪了

--- src/system/excel/exceljs.service.spec.ts
+++ src/system/excel/exceljs.service.spec.ts
@@ -3,50 +3,54 @@ import { INestApplication } from "@nestjs/common";
 import { ExcelJsService } from "./exceljs.service";
 import * as exceljs from "exceljs";
 
-describe('exceljs导出测试', () => {
-    let s_excel: ExcelJsService;
-    let app: INestApplication;
-    beforeAll(async () => {
-        const module = await Test.createTestingModule({
-            providers: [ExcelJsService],
-        }).compile();
-        app = module.createNestApplication();
-        await app.init();
-        s_excel = module.get(ExcelJsService);
-    });
+describe("exceljs导出测试", () => {
+  let s_excel: ExcelJsService;
+  let app: INestApplication;
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [ExcelJsService],
+    }).compile();
+    app = module.createNestApplication();
+    await app.init();
+    s_excel = module.get(ExcelJsService);
+  });
 
-    afterAll(async () => {
-        await app.close();
-    });
+  afterAll(async () => {
+    await app.close();
+  });
 
-    it('测试基础导出', async () => {
-        let workbook = new exceljs.Workbook();
-        workbook.xlsx.writeFile('public/excel/测试exceljs基础导出.xlsx');
-        debugger;
-        let datas = [];
-        for (let j = 0; j < 1000; j++) {
-            for (let i = 0; i < 1000; i++) {
-                datas.push({
-                    hello: '哈哈哈' + i,
-                    world: '黑乎乎' + i,
-                    hehe2: '黑乎乎' + i,
-                    hehe3: '黑乎乎' + i,
-                    hehe4: '黑乎乎' + i,
-                    hehe5: '黑乎乎' + i,
-                    hehe6: '黑乎乎' + i,
-                    hehe7: '黑乎乎' + i,
-                    hehe8: '黑乎乎' + i,
-                    hehe9: '黑乎乎' + i,
-                    hehe10: '黑乎乎' + i,
-                    hehe11: '黑乎乎' + i,
-                    hehe12: '黑乎乎' + i,
-                    hehe13: '黑乎乎' + i,
-                    hehe14: '黑乎乎' + i,
-                    hehe15: '黑乎乎' + i,
-                })
-            }
-            await s_excel.generate(datas, 'public/excel/测试exceljs基础导出.xlsx');
-        }
-        expect(true).toBe(true);
+  it("测试基础导出", async () => {
+    const workbook = new exceljs.stream.xlsx.WorkbookWriter({
+      filename: "public/excel/测试exceljs基础导出.xlsx",
     });
+    const sheet = workbook.addWorksheet("test");
+    for (let j = 0; j < 10; j++)
+      for (let i = 0; i < 100000; i++) {
+        sheet
+          .addRow([
+            "哈哈哈" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+            "黑乎乎" + i,
+          ])
+          .commit();
+      }
+
+    // await s_excel.add(sheet, datas);
+    sheet.commit();
+    await workbook.commit();
+    expect(true).toBe(true);
+  });
 });
--- src/system/excel/exceljs.service.ts
+++ src/system/excel/exceljs.service.ts
@@ -3,25 +3,19 @@ import * as ExcelJS from "exceljs";
 
 @Injectable()
 export class ExcelJsService {
-    private async add(datas: any[], filename: string) {
-        let workbook = new ExcelJS.Workbook();
-        await workbook.xlsx.readFile(filename);
-        let worksheet = workbook.getWorksheet(1);
-        let lastRow = worksheet.lastRow;
-        let num = lastRow.number;
-        worksheet.insertRows(num, datas);
-        worksheet.commit();
-    }
+  async add(worksheet, datas: any[]) {
+    for (let i = 0; i < datas.length; i++) worksheet.addRow(datas[i.toString()]).commit();
+    return true;
+  }
 
-
-    async generate(datas: any[], filePath: string) {
-        let chunk = []
-        let num = 10000;
-        let flag = false;
-        while (datas.length > 0) {
-            chunk = datas.splice(0, num)
-            await this.add(chunk, filePath);
-            flag = true
-        }
-    }
+  // async generate(datas: any[], filePath: string) {
+  //   let chunk = [];
+  //   let num = 10000;
+  //   let flag = false;
+  //   while (datas.length > 0) {
+  //     chunk = datas.splice(0, num);
+  //     await this.add(chunk, filePath);
+  //     flag = true;
+  //   }
+  // }
 }
